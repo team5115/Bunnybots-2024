ae5f207f471d78fb7d5798b7cb99c415799bd541
Alex G <alexg@silversat.org>
2024-10-02 16:01:37 -0400

    Fixed Robot.java errors

--- src/main/java/frc/team5115/Robot.java
+++ src/main/java/frc/team5115/Robot.java
@@ -27,49 +27,49 @@ public class Robot extends LoggedRobot {
     @Override
     public void robotInit() {
         // Record metadata
-        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
-        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
-        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
-        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
-        switch (BuildConstants.DIRTY) {
-            case 0:
-                Logger.recordMetadata("GitDirty", "All changes committed");
-                break;
-            case 1:
-                Logger.recordMetadata("GitDirty", "Uncomitted changes");
-                break;
-            default:
-                Logger.recordMetadata("GitDirty", "Unknown");
-                break;
-        }
+        // Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        // Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        // Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        // Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        // Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        // switch (BuildConstants.DIRTY) {
+        //     case 0:
+        //         Logger.recordMetadata("GitDirty", "All changes committed");
+        //         break;
+        //     case 1:
+        //         Logger.recordMetadata("GitDirty", "Uncomitted changes");
+        //         break;
+        //     default:
+        //         Logger.recordMetadata("GitDirty", "Unknown");
+        //         break;
+        // }
 
         // Set up data receivers & replay source
-        switch (Constants.currentMode) {
-            case REAL:
-                // Running on a real robot, log to a USB stick ("/U/logs")
-                String path = "/U/logs";
-                var directory = new File(path);
-                if (!directory.exists()) {
-                    directory.mkdir();
-                }
-                Logger.addDataReceiver(new WPILOGWriter(path));
-                Logger.addDataReceiver(new NT4Publisher());
-                break;
+        // switch (Constants.currentMode) {
+            // case REAL:
+            //     // Running on a real robot, log to a USB stick ("/U/logs")
+            //     String path = "/U/logs";
+            //     var directory = new File(path);
+            //     if (!directory.exists()) {
+            //         directory.mkdir();
+            //     }
+            //     Logger.addDataReceiver(new WPILOGWriter(path));
+            //     Logger.addDataReceiver(new NT4Publisher());
+            //     break;
 
-            case SIM:
-                // Running a physics simulator, log to NT
-                Logger.addDataReceiver(new NT4Publisher());
-                break;
+            // case SIM:
+            //     // Running a physics simulator, log to NT
+            //     Logger.addDataReceiver(new NT4Publisher());
+            //     break;
 
-            case REPLAY:
-                // Replaying a log, set up replay source
-                setUseTiming(false); // Run as fast as possible
-                String logPath = LogFileUtil.findReplayLog();
-                Logger.setReplaySource(new WPILOGReader(logPath));
-                Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
-                break;
-        }
+            // case REPLAY:
+            //     // Replaying a log, set up replay source
+            //     setUseTiming(false); // Run as fast as possible
+            //     String logPath = LogFileUtil.findReplayLog();
+            //     Logger.setReplaySource(new WPILOGReader(logPath));
+            //     Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+            //     break;
+        // }
 
         // See http://bit.ly/3YIzFZ6 for more information on timestamps in AdvantageKit.
         // Logger.disableDeterministicTimestamps()
@@ -90,8 +90,8 @@ public class Robot extends LoggedRobot {
         // finished or interrupted commands, and running subsystem periodic() methods.
         // This must be called from the robot's periodic block in order for anything in
         // the Command-based framework to work.
-        CommandScheduler.getInstance().run();
-        robotContainer.robotPeriodic();
+        // CommandScheduler.getInstance().run();
+        // robotContainer.robotPeriodic();
     }
 
     /** This function is called once when the robot is disabled. */
