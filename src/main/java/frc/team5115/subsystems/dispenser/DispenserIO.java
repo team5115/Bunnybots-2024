d828234dde4c3910f6035a57149480891a6c3364
v1x2 <viviaphan2008@gmail.com>
2024-11-06 15:33:37 -0500

    io

--- src/main/java/frc/team5115/subsystems/dispenser/DispenserIO.java
+++ src/main/java/frc/team5115/subsystems/dispenser/DispenserIO.java
@@ -1,5 +1,13 @@
 package frc.team5115.subsystems.dispenser;
+import org.littletonrobotics.junction.AutoLog;
 
-public class DispenserIO {
-    
+public interface DispenserIO {
+    @AutoLog
+    public static class DispenserIOInputs{
+        public double velocityRPM = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+     public default void updateInputs(DispenserIOInputs inputs) {}
+     public default void setDispenserVoltage(double volts) {}
 }
