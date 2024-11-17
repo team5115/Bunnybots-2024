39a709a655c95dfc3cc065c8f60abe33c67678f4
AvidCoder27 <gilireeves@gmail.com>
2024-11-17 10:37:28 -0500

    arm rework

--- src/main/java/frc/team5115/subsystems/arm/Arm.java
+++ src/main/java/frc/team5115/subsystems/arm/Arm.java
@@ -1,81 +1,139 @@
 package frc.team5115.subsystems.arm;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.team5115.Constants;
 import org.littletonrobotics.junction.Logger;
 
 public class Arm extends SubsystemBase {
+    // TODO determine max speed, accel, and volts for arm
+    private final double maxSpeedDegreesPerSecond = 175.0;
+    private final double maxAccelerationDegreesPerSecondPerSecond = 350.0;
+    private final double maxVolts = 10.0;
+
     private final ArmIO io;
     private final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
-    private final ArmFeedforward feedforward;
-    private final PIDController pid;
+    private final ArmFeedforward feedforward; // radians
+    private final ProfiledPIDController pid; // degrees
+    private final SysIdRoutine sysId;
+    private State state = State.INITIAL;
+
+    public enum State {
+        INITIAL(+91.0),
+        STOWED(+66.0),
+        INTAKE(-24.0),
+        DISPENSE(+135.0);
+
+        private final double position;
+
+        State(double degrees) {
+            position = degrees;
+        }
+    }
 
     public Arm(ArmIO io) {
         this.io = io;
-
+        final var constraints =
+                new TrapezoidProfile.Constraints(
+                        MetersPerSecond.of(maxSpeedDegreesPerSecond),
+                        MetersPerSecondPerSecond.of(maxAccelerationDegreesPerSecondPerSecond));
         switch (Constants.currentMode) {
             case REAL:
             case REPLAY:
+                // TODO tune arm feedforward and pid using sysid
                 feedforward = new ArmFeedforward(0.3, 0.35, 0.13509, 0.048686);
-                pid = new PIDController(0.405, 0.0, 0.0);
+                pid = new ProfiledPIDController(0.405, 0.0, 0.0, constraints);
                 break;
             case SIM:
-                feedforward = new ArmFeedforward(0.0, 0.35, 0.15, 0.0);
-                pid = new PIDController(1.0, 0.0, 0.0);
+                feedforward = new ArmFeedforward(0.0, 0.35, 0.135, 0.05);
+                pid = new ProfiledPIDController(0.3, 0.0, 0.0, constraints);
                 break;
             default:
                 feedforward = new ArmFeedforward(0.0, 0.0, 0, 0.0);
-                pid = new PIDController(0.0, 0.0, 0.0);
+                pid = new ProfiledPIDController(0.0, 0.0, 0.0, constraints);
                 break;
         }
 
+        sysId =
+                new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                null,
+                                null,
+                                null,
+                                (state) -> Logger.recordOutput("Arm/SysIdState", state.toString())),
+                        new SysIdRoutine.Mechanism(
+                                (voltage) -> io.setArmVoltage(voltage.magnitude()), null, this));
+
         pid.setTolerance(3.0);
-        pid.setSetpoint(91.0);
+        pid.setGoal(91.0);
     }
 
     @Override
     public void periodic() {
         io.updateInputs(inputs);
         Logger.processInputs("Arm", inputs);
-        Logger.recordOutput("Arm/Setpoint Degrees", pid.getSetpoint());
+        Logger.recordOutput("Arm/Goal Degrees", pid.getGoal().position);
         Logger.recordOutput("Arm/Actual Degrees", inputs.armAngle.getDegrees());
-        Logger.recordOutput("Arm/At Setpoint?", pid.atSetpoint());
+        Logger.recordOutput("Arm/At Goal?", pid.atGoal());
+        Logger.recordOutput("Arm/State", getStateString());
 
-        // Update the pids and feedforward
-        final double speed = pid.calculate(inputs.armAngle.getDegrees());
-        double voltage = feedforward.calculate(inputs.armAngle.getRadians(), speed);
-        voltage = MathUtil.clamp(voltage, -10, +10);
-
-        io.setArmVoltage(voltage);
+        // The feedforward uses the pid's trapezoidal setpoint to counteract gravity and kstatic
+        io.setArmVoltage(
+                MathUtil.clamp(
+                        pid.calculate(inputs.armAngle.getDegrees())
+                                + feedforward.calculate(
+                                        Math.toRadians(pid.getSetpoint().position),
+                                        Math.toRadians(pid.getSetpoint().velocity)),
+                        -maxVolts,
+                        +maxVolts));
     }
 
     public Command waitForSetpoint(double timeout) {
-        return Commands.waitUntil(() -> pid.atSetpoint()).withTimeout(timeout);
+        return Commands.waitUntil(() -> pid.atGoal()).withTimeout(timeout);
     }
 
-    public Command setAngle(Rotation2d setpoint) {
-        return Commands.runOnce(() -> pid.setSetpoint(setpoint.getDegrees()));
-    }
-
-    public Command goToAngle(Rotation2d setpoint, double timeout) {
-        return setAngle(setpoint).andThen(waitForSetpoint(timeout));
+    private Command setState(State state) {
+        return Commands.runOnce(
+                () -> {
+                    this.state = state;
+                    pid.setGoal(state.position);
+                });
     }
 
     public Command stow() {
-        return setAngle(Rotation2d.fromDegrees(+66.0));
+        return setState(State.STOWED);
     }
 
     public Command prepareDispense() {
-        return setAngle(Rotation2d.fromDegrees(+135.0));
+        return setState(State.DISPENSE);
     }
 
     public Command intake() {
-        return setAngle(Rotation2d.fromDegrees(-24.0));
+        return setState(State.INTAKE);
+    }
+
+    private String getStateString() {
+        if (pid.atGoal()) {
+            return state.toString();
+        } else {
+            return "MOVING_TO_" + state.toString();
+        }
+    }
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return sysId.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return sysId.dynamic(direction);
     }
 }
