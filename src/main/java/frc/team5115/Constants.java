af28ba859cb64b34f5bc58632c36c8fab8481986
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-12-04 15:47:52 -0500

    drivetrain constant  changes

--- src/main/java/frc/team5115/Constants.java
+++ src/main/java/frc/team5115/Constants.java
@@ -43,8 +43,8 @@ public final class Constants {
         public static final byte FRONT_RIGHT_TURN_ID = 7;
 
         public static final double MAX_LINEAR_SPEED = 4.2; // meters per second
-        public static final double TRACK_WIDTH_X = Units.inchesToMeters(23.75);
-        public static final double TRACK_WIDTH_Y = Units.inchesToMeters(23.75);
+        public static final double TRACK_WIDTH_X = .595; //meters
+        public static final double TRACK_WIDTH_Y = .595; //meters
         public static final double DRIVE_BASE_RADIUS =
                 Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
         public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
